0f3318b2705fca22d060835dec0483d84b5379d3
Rushikesh Wakhre <98548939+Rushiwakhre@users.noreply.github.com>
2024-10-03 13:30:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+// Back to top button
+const topButton = document.createElement('button');
+top
